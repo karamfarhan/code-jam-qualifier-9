1308dcfad64b23dfd6164cc1ef7a8ee45674dbb4
karamfarhan <www.karam777krm@gmail.com>
2022-07-13 22:21:11 +0300

    my solution

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -21,6 +21,7 @@ class RestaurantManager:
         """
         self.staff = {}
 
+
     async def __call__(self, request: Request):
         """Handle a request received.
 
@@ -31,4 +32,29 @@ class RestaurantManager:
             Request object containing information about the sent
             request to your application.
         """
-        ...
+
+        # add staff -- onduty
+        if request.scope["type"] == "staff.onduty":
+            self.staff[request.scope["id"]] = request
+
+        # remove staff -- offduty
+        if request.scope["type"] == "staff.offduty":
+            remove_staff = self.staff.pop(request.scope["id"],None)
+            if not remove_staff:
+                return False
+
+        # handle the order 
+        if request.scope["type"] == "order":
+            # select the specialized staff
+            select_staff = None
+            for key,val in self.staff.items():
+                if request.scope["speciality"] in val.scope["speciality"]:
+                    select_staff = val
+                    
+            # handdling the order and send it back
+            full_order = await request.receive()
+            await select_staff.send(full_order)
+            result = await select_staff.receive()
+            await request.send(result)
+
+
